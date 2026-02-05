8a8b024504a833e843d352ba2981da127aa88869
QuinnZhou-CA <18774892897@163.com>
2026-02-05 21:12:01 +0000

    update

--- public/sketch.js
+++ public/sketch.js
@@ -29,6 +29,15 @@ let rotateDegrees = 0;
 let frontToBack = 0;
 let leftToRight = 0;
 
+let x, y;
+let maxSnotSpeed = 8;
+let coinX,coinY;
+
+let coinSize = 40;
+let snotSize = 10;
+
+let count = 0;
+
 // throttle device motion sending
 let lastSent = 0;
 const SEND_RATE = 30; // ms (~33 fps)
@@ -38,8 +47,11 @@ function setup() {
   canvas.parent("sketch-container"); 
 
   //random position used for visualisation
-  randomX = random(50,width-50);
-  randomY = random(50,height-50);
+
+  x = width/2;
+  y = height/2;
+  coinX = random(0,width);
+  coinY = random(0, height);
 
   rectMode(CENTER);
   angleMode(DEGREES);
@@ -69,17 +81,17 @@ function setup() {
 }
 
 function draw() {
-  background(240);
+  background(255);
 
   //draw movers for everyone
   // console.log(experienceState.users);
   // draw all users including myself
   for (let id in experienceState.users) {
     //if I'm a moving device not a PC / laptop
-    if(experienceState.users[id].deviceMoves){
-      drawOthers(id);
-    }
-  }
+  //   if(experienceState.users[id].deviceMoves){
+  //     drawOthers(id);
+  //   }
+  // }
 
   // DESKTOP MESSAGE 
   if (!isMobileDevice) {
@@ -107,222 +119,85 @@ function draw() {
 // --------------------
 
 //visualise other drawing
-function drawOthers(id){
-  let u = experienceState.users[id];
-  let motion = u.motionData;
-  // console.log(u);
+// function drawOthers(id){
+//   let u = experienceState.users[id];
+//   let motion = u.motionData;
+//   // console.log(u);
 
-  let rectHeight = map(motion.orientation.beta, -90,90,0,height);//front to back is beta
-  // console.log(rectHeight,motion.orientation.beta);
+//   let rectHeight = map(motion.orientation.beta, -90,90,0,height);//front to back is beta
+//   // console.log(rectHeight,motion.orientation.beta);
 
-  fill(0,0,255,100);// slightly transparent
-  push();
-  rectMode(CORNER);
-  noStroke();
-  rect(motion.screenPosition.x,0,40,rectHeight);
-  pop();
+//   fill(0,0,255,100);// slightly transparent
+//   push();
+//   rectMode(CORNER);
+//   noStroke();
+//   rect(motion.screenPosition.x,0,40,rectHeight);
+//   pop();
 
-}
+// }
 
 
 function visualiseMyData(){
-//global variables
-let askButton;
+   // Simple movement threshold visualisation
+  let totalMovement =
+    Math.abs(accX) + Math.abs(accY) + Math.abs(accZ);
 
-// device motion
-let accX = 0;
-let accY = 0; 
-let accZ = 0;
-let rrateX = 0;
-let rrateY = 0; 
-let rrateZ = 0;
-
-// device orientation
-let rotateDegrees = 0;
-let frontToBack = 0; 
-let leftToRight = 0; 
-
-let x, y;
-let maxSnotSpeed = 8;
-let coinX,coinY;
-
-//let gotCoin = false;
-let coinSize = 40;
-let snotSize = 10;
-
-let count = 0;
-
-function setup() {
-  createCanvas(400, 400);
-  rectMode(CENTER);
-  angleMode(DEGREES);
-  
-  x = width/2;
-  y = height/2;
-  
-  coinX = random(0,width);
-  coinY = random(0, height);
-  //----------
-  //the bit between the two comment lines could be move to a three.js sketch except you'd need to create a button there
-  if(typeof DeviceMotionEvent.requestPermission === 'function' && typeof DeviceOrientationEvent.requestPermission === 'function'){
-    // iOS 13+
-    askButton = createButton('Permission');//p5 create button
-    askButton.mousePressed(handlePermissionButtonPressed);//p5 listen to mousePressed event
-  }else{
-    //if there is a device that doesn't require permission
-    window.addEventListener('devicemotion', deviceMotionHandler, true);
-    window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+  if (totalMovement > 2) {
+    background(255, 0, 0, 50); // make it slightly transparent
   }
-  
-  //----------
-  
-}
 
-//we are using p5.js to visualise this movement data
-function draw() {
-
-  
-  
-  let totalMovement = Math.abs(accX)+Math.abs(accY)+Math.abs(accZ);//movement in any direction
-  //set your own threshold for how sensitive you want this to be
-  
-  background(255);
-
-//   push();
-  circle(coinX, coinY, coinSize);
-//   pop();
-  
+  // Orientation arrows
   push();
-  stroke('rgb(217,238,171)');
-  strokeWeight(snotSize);
-  line(width/2, height/2, x,y);
-  pop();
-  
-  // if(totalMovement > 2){
-  //    //background(0,255,0);
-  // }else{
-  //    background(255);
-  // }
-  
-  //Creating a tilt sensor mechanic that has a sort of boolean logic (on or off)
-  //if the phone is rotated front/back/left/right we will get an arrow point in that direction 
-  push();
-  translate(width/2,height/2);
- 
-//   if(frontToBack > 40){ // down
-//     push();
-//     rotate(-180);
-//     triangle(-30,-40,0,-100,30,-40);
-//     //y = y + snotSpeed;
-//     pop();
-//   }else if(frontToBack < 0){ // up
-//     push();
-//     triangle(-30,-40,0,-100,30,-40);
-//     //y = y - snotSpeed;
-//     pop();
-//   }
-  
-//   if(leftToRight > 20){ // right
-//     push();
-//     rotate(90);
-//     triangle(-30,-40,0,-100,30,-40);
-//     //x = x + snotSpeed;
-//     pop();
-//   }else if(leftToRight < -20){ // left
-//     push();
-//     rotate(-90);
-//     triangle(-30,-40,0,-100,30,-40);
-//     //x = x- snotSpeed;
-//     pop();
-//   }
-  
-  x = x + constrain(map(leftToRight,-50,50,-maxSnotSpeed,maxSnotSpeed), -maxSnotSpeed, maxSnotSpeed);
-  
-   y = y + constrain(map(frontToBack,-30,50,-maxSnotSpeed,maxSnotSpeed), -maxSnotSpeed, maxSnotSpeed);
-  
-  
-  pop();
-
-  
-  
-  
-  
-  //Debug text
   fill(0);
-  textSize(15);
-  
-//   text("acceleration: ",10,10);
-//   text(accX.toFixed(2) +", "+accY.toFixed(2)+", "+accZ.toFixed(2),10,40);
+  translate(width / 2, height / 2);
 
-//   text("rotation rate: ",10,80);
-//   text(rrateX.toFixed(2) +", "+rrateY.toFixed(2)+", "+rrateZ.toFixed(2),10,110);
-  
-  
-  text("count: "+count,10,150); 
-  
-  checkCollision();
-  
-}
-
-function checkCollision() {
-  if(dist(coinX, coinY, x, y)< coinSize/2 + snotSize/2){
-  coinX = random(0,width);
-  coinY = random(0, height);
-  count = count+1;
+  if (frontToBack > 40) {
+    rotate(180);
+    triangle(-30, -40, 0, -100, 30, -40);
+  } else if (frontToBack < 0) {
+    triangle(-30, -40, 0, -100, 30, -40);
   }
-}
 
-//Everything below here you could move to a three.js or other javascript sketch
+  if (leftToRight > 20) {
+    rotate(90);
+    triangle(-30, -40, 0, -100, 30, -40);
+  } else if (leftToRight < -20) {
+    rotate(-90);
+    triangle(-30, -40, 0, -100, 30, -40);
+  }
 
-function handlePermissionButtonPressed(){
+  pop();
 
-    DeviceMotionEvent.requestPermission()
-    .then(response => {
-      // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
-      if (response === 'granted') {
-        window.addEventListener('devicemotion', deviceMotionHandler, true);
-      }
-    });
+  push();
+  fill(255);
+  rectMode(CORNER);
+  rect(0,20,width/2,190);
+  pop();
 
-    DeviceOrientationEvent.requestPermission()
-    .then(response => {
-      if (response === 'granted') {
-        // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
-        window.addEventListener('deviceorientation', deviceTurnedHandler, true)
-      }
-    })
-    .catch(console.error);  
-}
+  // Debug text
+  fill(0);
+  textAlign(LEFT);
 
-//AVERAGE YOUR DATA!!!
-//Microphone input from last term.... 
+  text("Acceleration:", 10, 40);
+  text(
+    accX.toFixed(2) + ", " + accY.toFixed(2)+ ", " + accZ.toFixed(2),
+    10,
+    60
+  );
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/devicemotion_event
-function deviceMotionHandler(event){
-  
-  accX = event.acceleration.x;
-  accY = event.acceleration.y;
-  accZ = event.acceleration.z;
-  
-  rrateZ = event.rotationRate.alpha;//alpha: rotation around z-axis
-  rrateX = event.rotationRate.beta;//rotating about its X axis; that is, front to back
-  rrateY = event.rotationRate.gamma;//rotating about its Y axis: left to right
-  
-}
+  text("Rotation rate:", 10, 100);
+  text(
+    rrateX.toFixed(2) + ", " + rrateY.toFixed(2) + ", " + rrateZ.toFixed(2),
+    10,
+    120
+  );
 
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/deviceorientation_event
-function deviceTurnedHandler(event){
-  
-  //degrees 0 - 365
-  rotateDegrees = event.alpha; // alpha: rotation around z-axis
-  frontToBack = event.beta; // beta: front back motion
-  leftToRight = event.gamma; // gamma: left to right
-
-}
-
-
-
-
+  text("Orientation:", 10, 160);
+  text(
+    rotateDegrees.toFixed(2) + ", " + leftToRight.toFixed(2) + ", " + frontToBack.toFixed(2),
+    10,
+    180
+  );
 }
 
 // SEND DATA TO SERVER
